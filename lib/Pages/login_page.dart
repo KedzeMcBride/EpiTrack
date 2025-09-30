4c70de1fd2dda035901e8dd2caf7af4ccb0f716c
Kedze Mcbride <kedzemcbridekum@gmail.com>
2025-09-30 03:20:01 -0700

    beginning of user login and authentication

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -11,24 +11,42 @@ class LoginPage extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
-              const SizedBox(height: 50,),
+              const SizedBox(height: 50),
               // logo
               Image.asset(
                 'lib/images/logo.png',
-                height: 100, // Adjust height as needed
-                width: 100,  // Adjust width as needed
+                height: 150, // Adjust height as needed
+                width: 150,  // Adjust width as needed
               ),
-              const SizedBox(height: 50,),
+              const SizedBox(height: 50),
 
               // welcome back
-                Text(
+              Text(
                 "Welcome Back You've been Missed",
                 style: TextStyle(
                   color: Colors.grey[700],
                   fontSize: 16,
                 ),
               ),
+              const SizedBox(height: 25),
+
               // user text-field
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    fillColor: Colors.grey.shade200,
+                    filled: true,
+                    hintText: 'Username',
+                  ),
+                ),
+              ),
 
               // password text-field
 
