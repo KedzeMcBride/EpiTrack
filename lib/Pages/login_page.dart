9a250db2a587122d5ab156810d24f8390e0efd22
Kedze Mcbride <kedzemcbridekum@gmail.com>
2025-09-30 02:40:38 -0700

    beginning of user login and authentication

---
+++ lib/Pages/login_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.grey,
+      body: Column(children: [
+        //logo
+
+        //welcome back
+
+        //user textfield
+
+        //password textfield
+
+        //forgot password
+
+        //sigin button
+
+        //or continue with
+
+        //google or apple
+      ],),
+    );
+  }
+}
